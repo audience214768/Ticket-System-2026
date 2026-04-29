8b16467fbce9f1361d09cc26700f00df96e90aa0
谢家烨 <audience@xiejiayedeMacBook.local>
2026-04-29 20:57:00 +0800

    fix : add the page num per file

--- include/common/config.h
+++ include/common/config.h
@@ -6,10 +6,10 @@ using page_id_t = size_t;
 using frame_id_t = size_t;
 
 constexpr int DISK_PAGE_SIZE = 4096;
-constexpr int DISK_FILE_SIZE = 8192;
-constexpr int FILE_BIT = 13;
+constexpr int DISK_FILE_SIZE = 8192 *4;
+constexpr int FILE_BIT = 13 + 2;
 constexpr page_id_t INVALID_PAGE_ID = -1;
 constexpr size_t INF_TIME_STAMP = -1;
 constexpr frame_id_t INVALID_FRAME_ID = -1; 
-constexpr size_t HASH_SIZE = 8192 * 2;
+constexpr size_t HASH_SIZE = DISK_FILE_SIZE * 4;
 
